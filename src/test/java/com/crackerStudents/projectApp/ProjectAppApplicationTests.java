0fd1de044e954459ae1d6b2c2681eb5f977dcaf3
Vitalja Misnjankin <47272421+VM1995@users.noreply.github.com>
2019-03-28 17:22:12 +0300

    database development on the Hibernate platform

--- src/test/java/com/crackerStudents/projectApp/ProjectAppApplicationTests.java
+++ src/test/java/com/crackerStudents/projectApp/ProjectAppApplicationTests.java
@@ -2,11 +2,11 @@ package com.crackerStudents.projectApp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+//@SpringBootTest
 public class ProjectAppApplicationTests {
 
 	@Test
